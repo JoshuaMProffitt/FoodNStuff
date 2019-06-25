aa5ac905c930fd64de2780bfe2ba978fa4aa9b8b
Joshua Proffitt <joshua.m.proffitt@gmail.com>
2019-06-25 14:07:46 -0400

    Added Create and stuff

---
+++ FoodNStuff.MVC/Controllers/CustomerController.cs
@@ -0,0 +1,67 @@
+﻿using FoodNStuff.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodNStuff.MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(_db.Customers.ToList());
+        }
+        //Get: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //Post: Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Customers.Add(customer);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+        //Get: Details {id}
+        public ActionResult Details (int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult
+                    (HttpStatusCode.BadRequest);
+            Customer customer = _db.Customers.Find(id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+        //Get: Customer Edit {id}
+        public ActionResult Edit (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult
+                (HttpStatusCode.BadRequest);
+            }
+            Customer customer = _db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+    }
+}
